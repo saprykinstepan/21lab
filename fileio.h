ef9f9267f7624f8cd10f35fb0f7f47a8e59f5c6e
Stepan <saprykinstepa442@gmail.com>
2026-01-19 16:53:28 +0300

    Update fileio.h

--- fileio.h
+++ fileio.h
@@ -3,16 +3,16 @@
 
 #include "database.h"
 
-// Загрузка с созданием тестовых данных
+// Загрузка с тестовыми данными
 int load_from_file(Employee* arr, int* size, char* filename);
 
 // Сохранение всего массива
 int save_to_file(Employee* arr, int size, char* filename);
 
-// Простое чтение файла
+// Чтение файла без тестовых данных
 int input_file(char* filename, Employee* arr);
 
-// Простая запись в файл
+// Запись в файл
 int output_file(char* filename, Employee* arr, int n);
 
 #endif
